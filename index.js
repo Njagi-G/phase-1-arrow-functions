4796c9f196181da166f7f12df0a14929d78c2f58
Njagi Githaiga <njagigithaiga@gmail.com>
2024-03-29 16:22:42 +0300

    Submitting_arrow-functions_lab

---
+++ index.js
@@ -0,0 +1,22 @@
+//Instruction 1:
+
+const divide = (a, b) => a/b;
+
+divide(2000, 100);
+console.log(divide(2000, 100));
+
+
+//Instruction 2:
+
+const square = (x) => x*x;
+
+square(2);
+console.log(square(2));
+
+
+//Instruction 3:
+
+const add = (a, b) => a+b;
+
+add(3, 4);
+console.log(add(3, 4));
